d7473bb84aa0870549c528a64c48740e3987422f
Lucio neves <luciocecilia20@gmail.com>
2024-07-10 10:54:31 -0400

    Desenvolvimento do arquivo database

---
+++ employee-api/src/config/database.js
@@ -0,0 +1,21 @@
+const { Pool, Query } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+pool.on('error', (err, client) => {
+    console.log('Unexpected error on idle client', err);
+    process.exit(-1)
+})
+
+pool.on('connect', () => {
+    console.log('Base de dados conectado com sucesso!');
+});
+
+module.exports = {
+    Query: (text, params) => pool.query(text, params),
+};
