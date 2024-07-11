8a6cf40286bca009e75de986d68a5904a4a13b7a
Lucio neves <luciocecilia20@gmail.com>
2024-07-11 17:27:43 -0400

    Desenvolvimento da rota updateEmployeeById

--- employee-api/src/controllers/employee.controller.js
+++ employee-api/src/controllers/employee.controller.js
@@ -30,3 +30,24 @@ exports.listAllEmployees = async(req, res) => {
      const response = await db.query('SELECT  * FROM employee ORDER BY name ASC');
      res.status(200).send(response.rows);
 };
+
+//=> Método responsável por listar um determinado 'Employee' po Id:
+exports.findEmployeeById = async(req, res) => {
+    const employeeId = req.params.id;
+    const response = await db.query('SELECT * FROM employee WHERE employee_id =$1', [employeeId]);
+    res.status(200).send(response.rows);
+};
+
+//=> Método reponsável por atualizar um determinado 'Employee'por Id:
+exports.updateEmployeeById = async(req, res) => {
+    const employeeId = req.params.id;
+    const { name, job_role, salary, birth, employee_registration } = req.body
+    
+    const response = await db.query(
+        'UPDATE employee SET name = $1, job_role = $2, salary = $3, birth = $4, employee_registration = $5 WHERE employee_id = $6',
+        [name, job_role, salary, birth, employee_registration, employeeId]
+    );
+
+    res.status(200).send({ message: 'Employee Updated Successfully!'})
+
+}
--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -16,6 +16,13 @@ router.post('/employees', employeeController.createEmployee);
 //=> Rota responsável por listar todos os 'Coloboradores': (GET): localhost:3000/api/employees
 router.get('/employees', employeeController.listAllEmployees);
 
+//=> Rota responsável por listar um determinado 'Colaborador' po Id: (GET): localhost:3000/api/employees/id
+router.get('/employees/:id', employeeController.findEmployeeById)
+
+// => Rota responsável por atualizar um determinado 'Colaborador(a) por Id: (PUT): localhost:3000/api/employees/id
+router.put('/employees/:id', employeeController.updateEmployeeById);
+
+
 module.exports = router;
 
 
